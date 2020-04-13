76847ce91b718047d25a10cfa1d860dbc4f247e4
JC Kim <jck@linecorp.com>
2020-04-13 23:53:05 +0900

    fix: 🐛 use side/line param of pr comment api

--- fix.py
+++ fix.py
@@ -21,7 +21,7 @@ with open(os.environ.get('GITHUB_EVENT_PATH')) as gh_event:
         json_data['pull_request']['base']['repo']['full_name']
     ).get_pull(json_data['number'])
     for file in pr.get_files():
-        for position, diff in enumerate(parse_patch(file.patch)):
+        for line, diff in enumerate(parse_patch(file.patch)):
             print(diff)
             for change in diff.changes:
                 fixed = fix(change.line)
@@ -30,5 +30,6 @@ with open(os.environ.get('GITHUB_EVENT_PATH')) as gh_event:
                         f"""```suggestion\n{fixed}\n```""",
                         pr.get_commits()[0],
                         file.filename,
-                        position
+                        None,
+                        "RIGHT", line
                     )
